021ee1d0719470d1f8f22057265473c345e96686
kanth2022 <manthasuryakanthsubhramanyaswamy.22.le.it@anits.edu.in>
2024-05-23 13:01:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quiz_app/QuestionAnswer.java
@@ -0,0 +1,25 @@
+package com.example.quiz_app;
+
+public class QuestionAnswer {
+
+    public static String question []={
+            "Ashes is a test series between which two countries?",
+            "Who is the first captain to win all icc trophies in cricket?",
+            "who won the world cup in 1983?",
+            "who scores most runs in an ipl season?",
+            "which player has most centuries in international cricket?"
+    };
+    public static String choices [][]={
+            {"India & Australia","India & England","India & Pakistan","Australia & England"},
+            {"Ms Dhoni","Ricky Ponting","Alastair Cook","Afridi"},
+            {"England" , "West Indies" , "India" , "Pakistan"},
+            {"David Warner" , "Virat Kohli" , "Steve Smith" , "Suresh Raina"},
+            {"Ricky Ponting" , "Kumar Sangakara" , "Virat Kohli" , "Sachin Tendulkar"}
+    };
+    public static String correctAnswers []={
+            "Australia & England" ,
+            "Ms Dhoni",
+            "India",
+            "Virat Kohli",
+            "Sachin Tendulkar"};
+}
